26eb95fbbdfc21e1f359b01027d59d3f6f561989
cecrigope <cecrigope@hotmail.com>
2019-04-15 01:46:25 -0400

    Added additional animation to tagline

--- controller.js
+++ controller.js
@@ -2,23 +2,23 @@ var app = angular.module('cecrigope', ['matchMedia']);
 app.controller('ctrl', function($scope, $interval, screenSize) {
 
   $scope.isScreenSize = screenSize.is;
-  // $scope.descriptions = ['web applications', 'VR experiences', 'chatbots', 'solutions'];
-  // $scope.description = $scope.descriptions[0];
-  // var seconds = 0;
-  //
-  // if (seconds <= 4) {
-  //   $interval(function() {
-  //     console.log(seconds);
-  //     seconds = seconds + 1;
-  //     if (seconds == 0) {
-  //       $scope.description = $scope.descriptions[0];
-  //     } else if (seconds == 1) {
-  //       $scope.description = $scope.descriptions[1];
-  //     } else if (seconds == 2) {
-  //       $scope.description = $scope.descriptions[2];
-  //     } else {
-  //       $scope.description = $scope.descriptions[3];
-  //     }
-  //   }, 1000);
-  // }
+  $scope.descriptions = ['web applications', 'VR experiences', 'chatbots', 'solutions'];
+  $scope.description = $scope.descriptions[0];
+  var seconds = 0;
+
+  if (seconds <= 4) {
+    $interval(function() {
+      console.log(seconds);
+      seconds = seconds + 1;
+      if (seconds == 0) {
+        $scope.description = $scope.descriptions[0];
+      } else if (seconds == 1) {
+        $scope.description = $scope.descriptions[1];
+      } else if (seconds == 2) {
+        $scope.description = $scope.descriptions[2];
+      } else {
+        $scope.description = $scope.descriptions[3];
+      }
+    }, 1000);
+  }
 });
